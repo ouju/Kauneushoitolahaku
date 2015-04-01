bc10e6eb1353c960c0c595fce93c3f1d293f45f6
Outi Jussila <outi.jussila@helsinki.fi>
2015-04-01 11:30:37 +0300

    Uusi test data

--- web/sql/add-test-data.sql
+++ web/sql/add-test-data.sql
@@ -1,6 +1,7 @@
 INSERT INTO Yritys (nimi, tunnus, salasana) VALUES ('Kaunola', 'kaunola', 'kaaaunista');
 INSERT INTO Yritys (nimi, tunnus, salasana) VALUES ('Lepola', 'lepola', 'ihq99');
 INSERT INTO Yritys (nimi, tunnus, salasana, sijainti) VALUES ('Venus', 'venus', 'veveve', 'Vantaa');
+INSERT INTO Yritys (nimi, tunnus, salasana, hintataso, sijainti, osoite) VALUES ('Relaxo', 'relaxo', 'sleepy', 'keskitaso', 'Vantaa', 'Merikuja 5');
 
 INSERT INTO Palvelu (nimi) VALUES ('Kampaaja');
 INSERT INTO Palvelu (nimi) VALUES ('P‰‰hieronta');
