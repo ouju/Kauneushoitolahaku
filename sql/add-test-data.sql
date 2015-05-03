16fc63f2c80e38c72f0569037a9c717010c3468f
Outi Jussila <outi.jussila@helsinki.fi>
2015-05-03 17:25:40 +0300

    Valmis dokumentaatio

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,8 +1,3 @@
-INSERT INTO Yritys (nimi) VALUES ('Kaunola');
-INSERT INTO Yritys (nimi) VALUES ('Lepola');
-INSERT INTO Yritys (nimi, sijainti) VALUES ('Venus', 'Vantaa');
-INSERT INTO Yritys (nimi, hintataso, sijainti, osoite) VALUES ('Relaxo', 'Keskitaso', 'Vantaa', 'Merikuja 5');
-
 INSERT INTO Tyontekija (tunnus, salasana) VALUES ('venus', 'veveve');
 
 INSERT INTO Tarjonta (nimi) VALUES ('Hieronta');
